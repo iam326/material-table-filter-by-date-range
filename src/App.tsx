20bdde03ddf20d6eeda0f15bd80104ca0d13035d
t.takahashi <takahashi.developer@gmail.com>
2020-08-02 00:11:51 +0900

    add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <h1>hello world</h1>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="absolute">
+        <Toolbar>
+          <Typography component="h1" variant="h6" color="inherit" noWrap>
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <h1>Hello World</h1>
+            </Paper>
+          </Grid>
+        </Container>
+      </main>
+    </div>
   );
 }
 
