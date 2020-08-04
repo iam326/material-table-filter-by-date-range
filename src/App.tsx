ead56144bd676f555aa2e913417a948d72993f8a
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-08-05 00:10:29 +0900

    add DataTable component

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import MaterialTable from 'material-table';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
 
 import Header from './components/Header';
-import FilterDatePicker from './components/FilterDatePicker';
+import DataTable from './components/DataTable';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,49 +31,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const tableData = [
-  {
-    couponId: '0001',
-    storeName: '秋葉原店',
-    couponName: '日用品 お得クーポン',
-    discountRate: 10,
-    startDate: '2020-08-01',
-    endDate: '2020-08-07',
-  },
-  {
-    couponId: '0002',
-    storeName: '秋葉原店',
-    couponName: '酒類 お得クーポン',
-    discountRate: 5,
-    startDate: '2020-08-10',
-    endDate: '2020-08-20',
-  },
-  {
-    couponId: '0003',
-    storeName: '秋葉原店',
-    couponName: '食料品 お得クーポン',
-    discountRate: 8,
-    startDate: '2020-08-01',
-    endDate: '2020-08-31',
-  },
-  {
-    couponId: '0004',
-    storeName: '秋葉原店',
-    couponName: '化粧品 お得クーポン',
-    discountRate: 15,
-    startDate: '2020-08-21',
-    endDate: '2020-08-31',
-  },
-  {
-    couponId: '0004',
-    storeName: '秋葉原店',
-    couponName: '全商品 お得クーポン',
-    discountRate: 3,
-    startDate: '2020-08-25',
-    endDate: '2020-08-26',
-  },
-];
-
 const App: React.FC = () => {
   const classes = useStyles();
 
@@ -89,65 +43,7 @@ const App: React.FC = () => {
         <Container maxWidth="lg" className={classes.container}>
           <Grid item xs={12}>
             <Paper className={classes.paper}>
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <MaterialTable
-                  options={{
-                    search: false,
-                    draggable: false,
-                    filtering: true,
-                  }}
-                  columns={[
-                    {
-                      title: 'クーポン ID',
-                      field: 'couponId',
-                    },
-                    { title: '店舗', field: 'storeName' },
-                    {
-                      title: 'クーポン名',
-                      field: 'couponName',
-                    },
-                    {
-                      title: '割引率',
-                      field: 'discountRate',
-                      cellStyle: { textAlign: 'right' },
-                    },
-                    {
-                      title: '利用開始日',
-                      field: 'startDate',
-                      cellStyle: { textAlign: 'right' },
-                      filterComponent: (props) => (
-                        <FilterDatePicker
-                          columnDef={props.columnDef}
-                          onFilterChanged={props.onFilterChanged}
-                          label={'から'}
-                        />
-                      ),
-                      customFilterAndSearch: (
-                        filterValue: string | null,
-                        rowData: { startDate: string }
-                      ) => !filterValue || rowData.startDate >= filterValue,
-                    },
-                    {
-                      title: '利用終了日',
-                      field: 'endDate',
-                      cellStyle: { textAlign: 'right' },
-                      filterComponent: (props) => (
-                        <FilterDatePicker
-                          columnDef={props.columnDef}
-                          onFilterChanged={props.onFilterChanged}
-                          label={'まで'}
-                        />
-                      ),
-                      customFilterAndSearch: (
-                        filterValue: string | null,
-                        rowData: { endDate: string }
-                      ) => !filterValue || rowData.endDate <= filterValue,
-                    },
-                  ]}
-                  data={tableData}
-                  title="クーポン一覧"
-                />
-              </MuiPickersUtilsProvider>
+              <DataTable />
             </Paper>
           </Grid>
         </Container>
